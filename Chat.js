6755db1401525ff866e2a1babab0dde129f2cc9d
codemekh@gmail.com <codemekh@gmail.com>
2019-10-16 12:48:56 +0700

    Добавлена обработка post запроса регистрации пользователя. Привязал к БД

--- Chat.js
+++ Chat.js
@@ -1,15 +1,127 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+//---------- Сервер --------------------------------------------------------------
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const upload = require('express-fileupload');
+
+app.use(upload());
 
 server.listen(3000);
 
 app.use(express.static(__dirname));
 
+//----------------------- Обработка запросов --------------------------------------
+//----------------------- GET -------------
+
 app.get('/', function (request, response){
   response.sendFile(__dirname + '/index.html');
 });
+
+//----------------------- POST -------------
+
+app.post('/createAccount', function (request, response){
+  //TODO: добавить проверки на то что приходит.
+
+  let login = request.body.login;
+  let password = request.body.password;
+  let file = request.files.filename;
+
+  CreateAccount(login, password);
+  SetAvatar(login, file);
+
+  response.send("AccountCreatedSuccessfully");
+});
+
+//---------- База данных --------------------------------------------------------------
+
+const mysql = require('mysql');
+
+const options = {
+  user: 'root',
+  password: '',
+  database: 'chat'
+}
+
+const connection = mysql.createConnection(options)
+
+connection.connect(error => {
+  if (error)
+  {
+    LogMessage('An error occurred while connecting to the DB');
+    throw error;
+  }
+  else
+  {
+    LogMessage("Успешное подключение к БД");
+  }
+});
+
+//---------- Функции сервера --------------------------------------------------------------
+
+function GetLogTime(){
+  let date = new Date();
+  return '['+ date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ':' + date.getMilliseconds() + ']: ';
+}
+
+function LogMessage(message){
+  console.log(GetLogTime() + message);
+}
+
+//Получить расширение файла без точки. Прим.: jpg
+function GetFileExtension(filename){
+  return filename.split('.').pop();
+}
+
+//Зарегистрировать новый аккаунт
+function CreateAccount(login, password){
+  //...проверки
+  //TODO: Обязательно валидацию логина!!
+  //...
+  connection.query('INSERT INTO `users` (`login`, `password`, `avatar`, `id`) VALUES (?, ?, NULL, NULL);',[login, password], function(error){
+    if(error)
+    {
+      LogMessage("Ошибка регистрации пользователя: " + error);
+      //TODO: Добавить throw и обработчик ошибки добавления пользователя
+    }
+    else
+    {
+      LogMessage("Создан аккаунт [login: " + login + "]")
+    }
+  });
+}
+
+//Установить аватар для пользователя под ником login
+function SetAvatar(login, file){
+  let extension = GetFileExtension(file.name);
+  let filePath = "avatars/" + login + '.' + extension;
+  file.mv(filePath, function(error){
+    if(error)
+    {
+      LogMessage("Ошибка добавления пользователем [" + login + "] аватара [" + file.name + "]\n\t" + error);
+      //TODO: Добавить throw и обработчик ошибки
+    }
+    else
+    {
+      LogMessage("Пользователь [" + login + "] установил новый аватар [" + file.name + "]");
+    }
+  });
+
+  connection.query('UPDATE `users` SET `avatar` = ? WHERE `users`.`login` = 1;', [filePath], function(error){
+    if(error)
+    {
+      LogMessage("Ошибка добавления аватара пользователю [" + login + "]: " + error);
+      //TODO: Добавить throw и обработчик ошибки
+    }
+    else
+    {
+      LogMessage("Добавлен аватар для пользователя [login: " + login + "]")
+    }
+  });
+};
+
+//------------------------ События --------------------------------------------------------------------
 
 //Юзеры в чате //TODO: запилить фитчу для логов.
 users = [];
@@ -17,20 +129,25 @@ users = [];
 connections = [];
 
 io.sockets.on('connection', function(socket){
-  console.log("Новый юзер подключился");
+  LogMessage("Кто-то подключился");
   connections.push(socket);
 
   //----------события для каждого нового сокета-----------
 
   socket.on('disconnect', function(data){
     connections.splice(connections.indexOf(socket), 1);
-    console.log("Юзер отключился");
+    LogMessage("Юзер отключился");
   });
 
   socket.on('send_message', function(data){
     //вызываем событие и передаем в событие объект
     io.sockets.emit('add_new_message', {message : data.message, login : data.login});
-    console.log(data.login + " Отправил сообщение");
+    LogMessage(data.login + " Отправил сообщение");
   });
 
+});
+
+process.on('exit', function () {
+  connection.end();
+  // TODO: замутить сохранение БД
 });
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,19 @@
-const defaultLogin = "Безликий хуйлыга";
-var login = defaultLogin;
-var socket = io.connect();
-var form = $("#message_form");
-var textarea = $("#message");
-var messages = $("#all_messages");
+//------------------ Переменные -----------------------------
 
-socket.on('add_new_message', function(data){
-  messages.append("<div>" + data.login + ": " + data.message + "<div>");
-});
+const defaultLogin = "КОТ";
+const imageExtensions = ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'bmp', 'dib'];
+const socket = io.connect();
 
-function test(){
-  socket.emit('send_message', {message : textarea.val(), login : getLogin()});
-  textarea.val('');
-}
-
+//----------------- Функции ----------------------------------
 function setLogin(){
   login = document.getElementById("login_input").value;
-  if(login!=""){
+  if(login!="")
+  {
     document.getElementById("login").innerHTML += "Здесь вы известны под именем: "+login;
     turnOffOverlay();
   }
-  else {
+  else
+  {
     alert("НУ ПРИДУМАЙ, ЧО СЛОЖНО ЧТО ЛИ?");
     login = defaultLogin;
   }
@@ -33,3 +26,56 @@ function getLogin(){
 function turnOffOverlay(){
   document.getElementById("overlay").style.display = "none";
 }
+
+
+//------------------- Взаимодействие с беком -----------------------------------
+
+//Создать новый аккаунт. Возвращает true если все переданные перменные валидируются,
+//после чего отправляет их на сервер. В случае не прохождения валидации одной из переменных,
+//функция вернет false и ничего не отправит. для onsubmit. Action: /createAccount
+function Register(login, password, avatar){
+  if(IsFileImage(avatar)/*TODO: Добавить валидацию паролей и логинов*/)
+  {
+    socket.emit('add_new_user', {login: login, password: password, avatar: avatar})
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+//Отпправить новое сообщение
+function SendMessage(){
+  socket.emit('send_message', {message: textarea.val(), login: getLogin()});
+  textarea.val('');
+}
+
+//Если расширение файла содержится в списке допустимых расширений изображений, вернет true
+function IsFileImage(filename){
+  if(imageExtensions.includes( GetFileExtension(filename) ))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+
+}
+
+//Получить расширение файла без точки. Прим.: jpg
+function GetFileExtension(filename){
+  return filename.split('.').pop();
+}
+
+//---------------------------
+
+var login = defaultLogin;
+var form = $("#message_form");
+var textarea = $("#message");
+var messages = $("#all_messages");
+
+socket.on('add_new_message', function(data){
+  messages.append("<div>" + data.login + ": " + data.message + "<div>");
+});
